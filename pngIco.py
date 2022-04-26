9b9305a7c072d8316ce96e8901f5d95b4fca0318
kiwi-fruitiwi <zane.tian@gmail.com>
2022-04-26 13:17:29 -0700

    add ztable.py: graph normal distribution, add inputloop for ztable query. add ico png conversion

--- pngIco.py
+++ pngIco.py
@@ -1,15 +1,43 @@
 from PIL import Image
 import os
 
-# assign directory
-directory = 'img'
 
-# iterate over files in directory
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
+def scanDirConvert() -> None:
+    """
+    converts all files in /img to .ico format onto the user's desktop,
+    then removes the files
+    :return:
+    """
+    directory = 'img'
 
-    # checking if it is a file; if so → convert to .ico
-    if os.path.isfile(f):
-        print(f'{filename}')
-        img = Image.open(f)
-        img.save(f'out/{filename}.ico', format='ICO', sizes=[(256, 256)])
+    for filename in os.scandir(directory):
+        if filename.is_file():
+            n = filename.name
+            name_no_ext = n.split('.')[0]
+
+            print(name_no_ext)
+
+            # convert images to 256x256 .ico files
+            img = Image.open(filename.path)
+
+            out_path = f'{os.path.expanduser("~")}/Desktop/{name_no_ext}.ico'
+            print(out_path)
+            img.save(out_path, format="ICO", sizes=[(256, 256)])
+
+            # remove file
+            os.remove(filename.path)
+
+
+def listDirConvert():
+    directory = 'img'
+    for filename in os.listdir(directory):
+        f = os.path.join(directory, filename)
+
+        # checking if it is a file; if so → convert to .ico
+        if os.path.isfile(f):
+            print(f'{filename}')
+            img = Image.open(f)
+            img.save(f'out/{filename}.ico', format='ICO', sizes=[(256, 256)])
+
+
+scanDirConvert()
---
+++ ztable.py
@@ -0,0 +1,33 @@
+import math
+
+import numpy as np
+from scipy.integrate import quad
+
+from scipy.integrate import quad
+import matplotlib.pyplot as plt
+
+
+def graphNormalDistribution():
+    plt.style.use('dark_background')
+    x = np.linspace(-4, 4, num=100)
+    constant = 1.0 / np.sqrt(2 * np.pi)
+    pdf_normal_distribution = constant * np.exp((-x ** 2) / 2.0)
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(x, pdf_normal_distribution)
+    ax.set_ylim(0)
+    ax.set_title('Normal Distribution', size=20)
+    ax.set_ylabel('Probability Density', size=20)
+
+    plt.show()
+
+
+def normalProbabilityDensity(x):
+    coefficient = 1.0 / math.sqrt(2 * math.pi)
+    return coefficient * math.exp((-x ** 2)/ 2.0)
+
+
+while True:
+    lower_bound = float(input('enter lower integration bound'))
+    upper_bound = float(input('enter upper integration bound'))
+    print(quad(normalProbabilityDensity, lower_bound, upper_bound))
+
