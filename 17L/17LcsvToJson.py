9b48d2009e7d7ff9396d10281a8ed9d0e23e5389
kiwi <zane.tian@gmail.com>
2023-06-06 15:14:15 -0700

    properly convert csv to JSON output file with utf-8-sig

--- 17L/17LcsvToJson.py
+++ 17L/17LcsvToJson.py
@@ -1,4 +1,5 @@
 # from: askpython.com/python/examples/convert-csv-to-json
+# purpose: convert 17L csv data into JSON format for 🚂17Lgrades
 
 import csv
 import json
@@ -10,16 +11,19 @@ def csv_to_json(csvPath, jsonPath):
 
 	# Step 2
 	# open a csv file handler
-	with open(csvPath, encoding='utf-8') as csv_file_handler:
+	with open(csvPath, encoding='utf-8-sig') as csv_file_handler:
 		csv_reader = csv.DictReader(csv_file_handler)
 
 		# convert each row into a dictionary
 		# and add the converted data to the data_variable
 
-		for rows in csv_reader:
+		for row in csv_reader:
 			# primary key is the name of the file, 0
-			key = rows[0]
-			data_dict[key] = rows
+			key = row['Name']
+			data_dict[key] = row
+			# print(f'{key}')
+
+			print(f'{row}')
 
 	# open a json file handler and use json.dumps
 	# method to dump the data
@@ -29,12 +33,4 @@ def csv_to_json(csvPath, jsonPath):
 		json_file_handler.write(json.dumps(data_dict, indent=4))
 
 
-# driver code
-# be careful while providing the path of the csv file
-# provide the file path relative to your machine
-
-# Step 1
-csv_file_path = input('Enter the absolute path of the CSV file: ')
-json_file_path = input('Enter the absolute path of the JSON file: ')
-
-csv_to_json(csv_file_path, json_file_path)
+csv_to_json('ratings.csv', 'ratings.json')
