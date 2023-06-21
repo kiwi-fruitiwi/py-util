df265dc60f68ba07a2129e008626d15421891616
kiwi <zane.tian@gmail.com>
2023-06-21 16:53:32 -0700

    create a list of card names based on fuzzy matching on user input

--- 17L/fuzzyMatch.py
+++ 17L/fuzzyMatch.py
@@ -1,5 +1,6 @@
 import Levenshtein # makes finding the L-distance between strings much faster
 from fuzzywuzzy import process
+from typing import List
 import json
 
 # load json from our 17L csv to json converter
@@ -9,7 +10,31 @@ with open('ratings.json') as file:
 done: bool = False
 
 while not done:
-    userInput: str = input('\nEnter card name to match: ')
+    userInput: str = input('\nEnter cards: ')
 
-    bestMatches = process.extractBests(userInput, data.keys())
-    [print(e) for e in bestMatches]
+    # split the input string into a list using ',' as delimiter
+    names = userInput.split(',')
+
+    # trim leading and trailing whitespace
+    values = [name.strip() for name in names]
+
+    # set up list of card names matched to our input
+    cardFetchList: List[str] = []
+
+    for value in values:
+        # extractBests returns a list of tuples
+        #   → [print(e) for e in bestMatches] results in the following:
+        #       ('Aragorn, the Uniter', 90)
+        #       ('Gimli, Counter of Kills', 75)
+        #       ('Legolas, Counter of Kills', 75)
+        #       ('Mordor Muster', 66)
+        #       ('Bitter Downfall', 60)
+        bestMatches = process.extractBests(value, data.keys())
+        bestMatchTuple = bestMatches[0]
+
+        # we want the name part of the best match. we could use extractOne :P
+        bestMatchName = bestMatchTuple[0]
+        cardFetchList.append(bestMatchName)
+
+    # use cardFetchList to grab JSON data from data variable
+    [print(name) for name in cardFetchList]
