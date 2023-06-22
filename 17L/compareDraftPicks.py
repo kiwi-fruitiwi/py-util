1d6bd0adf036093a5768e0848814de0689a9846a
kiwi <zane.tian@gmail.com>
2023-06-22 08:40:14 -0700

    add stripped mana cost to CDP output

--- 17L/compareDraftPicks.py
+++ 17L/compareDraftPicks.py
@@ -30,7 +30,7 @@ gradeBounds: List[tuple] = [
 
 
 # main input loop to ask for user input → return list of card stats
-def main(data):
+def main(json17L, jsonScryfall):
 	done: bool = False
 	while not done:
 		userInput: str = input('\nEnter cards: ')
@@ -47,7 +47,7 @@ def main(data):
 		for value in values:
 			# extractOne returns a tuple like this: ('Arwen Undómiel', 90)
 			# we're just interested in the name, not the closeness
-			bestMatch = process.extractOne(value, data.keys())
+			bestMatch = process.extractOne(value, json17L.keys())
 
 			if bestMatch:
 				bestMatchName = bestMatch[0]
@@ -57,12 +57,13 @@ def main(data):
 
 		# use cardFetchList to grab JSON data from data variable
 		# [print(name) for name in cardFetchList]
-		printCardData(cardFetchList, data)
+		print(cardFetchList)
+		printCardData(cardFetchList, json17L, jsonScryfall)
 
 
 # get card data from data/ratings.json and display it for each cardName in
 # cardNameList :D If the JSON is sorted by GIHWR, so will the results.
-def printCardData(cardNameList: List[str], jsonData):
+def printCardData(cardNameList: List[str], json17L, jsonScryfall):
 	# load JSON converted from 17L csv export, where each entry looks like this:
 	# 	"Sunfall": {
 	#		"Name": "Sunfall",
@@ -85,7 +86,7 @@ def printCardData(cardNameList: List[str], jsonData):
 
 
 	# iterate through JSON data to determine σ and μ for data set first
-	for cardName in jsonData.keys():
+	for cardName in json17L.keys():
 		# the data is actually in string format: e.g. "GIH WR": "67.0%",
 		# so we need to do the following to convert to decimal:
 		#   verify right-most char is '%', then cast to float with 'e-2'
@@ -95,7 +96,7 @@ def printCardData(cardNameList: List[str], jsonData):
 		#   invasion of arcavios
 		#   jin-gitaxias, core augur
 		# we use None and test for that later if data is not present
-		winRateString: str = jsonData[cardName]["GIH WR"]
+		winRateString: str = json17L[cardName]["GIH WR"]
 		if winRateString != '':
 			assert winRateString[-1] == '%'
 			wr = float(winRateString.replace('%', 'e-2'))
@@ -110,16 +111,16 @@ def printCardData(cardNameList: List[str], jsonData):
 	σ: float = statistics.stdev(filteredWRs)
 
 	# header
-	print(f'\n   zscore  ata  ohwr gihwr    iwd   r      μ:{μ:.3f}, σ:{σ:.3f}')
+	print(f'\n   zscore  ata  ohwr gihwr    iwd   r       μ:{μ:.3f}, σ:{σ:.3f}')
 	# print(f'------------------------------------------------------------')
 
 	# now that we have the GIH WR σ and μ, display data:
 	# note the JSON will be sorted however it was when the csv was requested
 	# by default it will be by collector ID: alphabetical in wubrg order
-	for cardName in jsonData.keys():
+	for cardName in json17L.keys():
 		# some cards don't have data: check if it's actually in our GIHWR dict
 		if (cardName in cardWinRates) and (cardName in cardNameList):
-			cardData = jsonData[cardName] # card data json object
+			cardData = json17L[cardName] # card data json object
 			x: float = cardWinRates[cardName]  # GIH WR
 			color: str = cardData["Color"]
 			rarity: str = cardData["Rarity"]
@@ -164,12 +165,17 @@ def printCardData(cardNameList: List[str], jsonData):
 				ata: str = float(cardData["ATA"])
 
 				# if GIH WR exists, OH WR should too, so no extra check needed
-				ohwrStr:str = jsonData[cardName]["OH WR"]
+				ohwrStr: str = json17L[cardName]["OH WR"]
 
 				if ohwrStr != '':
 					ohwr: float = float(cardData["OH WR"].replace('%', 'e-2'))
 				else:
 					ohwr = 0  # temp value to indicate it's not available
+
+				# grab the mana cost from our collapsed scryfall dictionary:
+				# format is [cardName, mana cost] where latter is formatted
+				# 1UUU instead of {1}{U}{U}{U}
+				manacost: str = jsonScryfall[cardName]
 
 				# each row
 				print(
@@ -179,14 +185,35 @@ def printCardData(cardNameList: List[str], jsonData):
 					f'{ohwr*100:4.1f}% '
 					f'{cardWinRates[cardName]*100:4.1f}% '
 					f'{iwd:>6} '
-					f'← {rarity} {color:4} {cardName}')
+					f'← {rarity} {manacost:5} {cardName}')
 			else:
 				print(
 					f'insufficient data: {rarity} [{color}] {cardName}')
+
+
+# generates a dictionary mapping card names to their mana costs in format '2UUU'
+def generateNameManacostDict(sfJson):
+	# iterate through scryfallData. for each object:
+	#   strip {} from castingCost in format "{4}{G}{W}"
+	#   execute results[name] = strippedCastingCost
+	# return results
+	results: Dict[str, str] = {}
+	for card in sfJson:
+		# strip {}, converting {2}{W}{R} to 2WR
+		manaCost: str = card['mana_cost'].replace("{", "").replace("}", "")
+		name: str = card['name']
+		results[name] = manaCost
+
+	return results
 
 
 # load json from our 17L csv to json converter
 with open('data/ratings.json') as file:
 	data17Ljson = json.load(file)
 
-main(data17Ljson)
+# load card info from scryfall json
+with open('scryfall-ltr.json', encoding='utf-8-sig') as file:
+	scryfallData = json.load(file)
+
+
+main(data17Ljson, generateNameManacostDict(scryfallData))
