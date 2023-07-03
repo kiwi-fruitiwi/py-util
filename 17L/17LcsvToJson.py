47d8c92119b396ace570405becdc155e6d9d7094
kiwi <zane.tian@gmail.com>
2023-07-03 05:21:31 -0700

    update 17L data

--- 17L/17LcsvToJson.py
+++ 17L/17LcsvToJson.py
@@ -34,5 +34,5 @@ def csvToJSON(csvPath, jsonPath):
 		json_file_handler.write(json.dumps(cards, indent=4))
 
 
-csvToJSON('data/ltr/card-ratings-2023-07-02.csv', 'data/ratings.json')
+csvToJSON('data/ltr/card-ratings-2023-07-03.csv', 'data/ratings.json')
 # csvToJSON('mom/card-ratings-2023-06-21.csv', 'data/ratings.json')
