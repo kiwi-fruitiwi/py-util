e62d697a5c5a10755e6833c5eb6c7952f741b7b7
kiwi <zane.tian@gmail.com>
2023-07-03 18:39:11 -0700

    add comments to 17LrequestDemo

--- 17L/17LdataFetch.py
+++ 17L/17LdataFetch.py
@@ -35,34 +35,3 @@ colorPairs: List[str] = [
 allColors = requests.get(defaultUrl).json()
 with open('data/ltr-auto/all.json', 'w', encoding='utf-8') as json_file_handler:
 	json_file_handler.write(json.dumps(allColors, indent=4))
-
-'''
-sample JSON element from 17L request:
-{
-	"seen_count": 6171,
-	"avg_seen": 5.210338680926916,
-	"pick_count": 853,
-	"avg_pick": 7.561547479484173,
-	"game_count": 373,
-	"win_rate": 0.46380697050938335,
-	"sideboard_game_count": 60,
-	"sideboard_win_rate": 0.36666666666666664,
-	"opening_hand_game_count": 67,
-	"opening_hand_win_rate": 0.4925373134328358,
-	"drawn_game_count": 109,
-	"drawn_win_rate": 0.46788990825688076,
-	"ever_drawn_game_count": 176,
-	"ever_drawn_win_rate": 0.4772727272727273,
-	"never_drawn_game_count": 195,
-	"never_drawn_win_rate": 0.4512820512820513,
-	"drawn_improvement_win_rate": 0.025990675990676004,
-	"name": "Banish from Edoras",
-	"color": "W",
-	"rarity": "common",
-	"url": "https://cards.scryfall.io/border_crop/front/a/4/a4410076-e1fe-45f3-a0ca-a91ab0133ff4.jpg?1686397326",
-	"url_back": "",
-	"types": [
-		"Sorcery"
-	]
-},
-'''
--- 17L/17LrequestDemo.py
+++ 17L/17LrequestDemo.py
@@ -1,3 +1,7 @@
+# makes a 17L request and prints out the json to console
+# these are hidden by default on 17lands.com, but you can look at requests under
+# the network tab under the Chrome developer console
+
 import requests
 import json
 
@@ -10,3 +14,35 @@ response = requests.get(url)
 data = response.json()
 
 print(json.dumps(data, indent=3))
+
+
+'''
+sample JSON element from 17L request:
+{
+	"seen_count": 6171,
+	"avg_seen": 5.210338680926916,
+	"pick_count": 853,
+	"avg_pick": 7.561547479484173,
+	"game_count": 373,
+	"win_rate": 0.46380697050938335,
+	"sideboard_game_count": 60,
+	"sideboard_win_rate": 0.36666666666666664,
+	"opening_hand_game_count": 67,
+	"opening_hand_win_rate": 0.4925373134328358,
+	"drawn_game_count": 109,
+	"drawn_win_rate": 0.46788990825688076,
+	"ever_drawn_game_count": 176,
+	"ever_drawn_win_rate": 0.4772727272727273,
+	"never_drawn_game_count": 195,
+	"never_drawn_win_rate": 0.4512820512820513,
+	"drawn_improvement_win_rate": 0.025990675990676004,
+	"name": "Banish from Edoras",
+	"color": "W",
+	"rarity": "common",
+	"url": "https://cards.scryfall.io/border_crop/front/a/4/a4410076-e1fe-45f3-a0ca-a91ab0133ff4.jpg?1686397326",
+	"url_back": "",
+	"types": [
+		"Sorcery"
+	]
+},
+'''
