33c79b827af93c4de86b459894d0b4b1bacd49b4
kiwi <zane.tian@gmail.com>
2023-08-02 12:47:20 -0700

    understand and add division

--- nand2tetrisOS/math.py
+++ nand2tetrisOS/math.py
@@ -43,8 +43,45 @@ def getIthBit(number: int, i: int) -> int:
 	return int(result)
 
 
-target: int = 14
-for index in range(16):
-	print(f'the {index}-th bit of {target} is {getIthBit(target, index)}')
+# eyeball test for multiply methods
+def multTest():
+	target: int = 14
+	for index in range(16):
+		print(f'the {index}-th bit of {target} is {getIthBit(target, index)}')
 
-print(f'{multiply(0, 0)}')
+	print(f'{multiply(0, 0)}')
+
+
+# log n runtime division algorithm
+# uses the recursive logic → x/y is equal to twice of x/2y
+# 	plus some adjustment for large remainders
+def divide(dividend: int, divisor: int) -> int:
+	"""
+	:param dividend: the number we're dividing
+	:param divisor: what we're dividing by
+	:return: result of integer division: dividend/divisor
+	"""
+	assert dividend != 0
+
+	if divisor > dividend:
+		return 0
+
+	quotient: int = divide(dividend, 2 * divisor)
+	remainder: int = dividend - 2 * divisor * quotient
+
+	if remainder >= divisor:
+		return 2 * quotient + 1
+	else:
+		return 2 * quotient
+
+
+# eyeball test for divide method
+# 🐛 range started at 0. exceeded recursion limit because division by zero
+# 🐛 checking remainder > divisor instead of remainder >= divisor
+def divideTest():
+	dividend: int = 32
+	for index in range(1, 17):
+		print(f'🐳 {dividend}/{index}={divide(dividend, index)}')
+
+
+divideTest()
