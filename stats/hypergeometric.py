feca3110cf0a057be1764758f6ffa80c4c51ed8a
kiwi <zane.tian@gmail.com>
2023-08-15 18:32:31 -0700

    plot cumulative probability of drawing n lands. add dark mode to plot. rename hyperGeo PMF parameters to be magic based

--- stats/hypergeometric.py
+++ stats/hypergeometric.py
@@ -1,53 +1,69 @@
 # see docs.scipy.org/doc/scipy/reference/generated/scipy.stats.hypergeom.html
 
-from scipy.special import comb
+import numpy as np
+import matplotlib.pyplot as plt
 import scipy.stats as ss
+from scipy.special import comb
+from typing import List
 
 
-def hyperSingle(populationSize, sampleSize, successesInPop, successesInSample):
+def hypergeoPMF(deckSize, drawSteps, hits, k):
     """
     returns probability of n successes with popSize, sampleSize, successInPop
-    :param populationSize: total number of cards remaining in deck
-    :param sampleSize: number of draws
-    :param successesInPop: successes in population. 15 lands remaining in deck
-    :param successesInSample: successes we want in draws
-    :return: probability of n successes in
+    :param deckSize: total number of cards remaining in deck
+    :param drawSteps: number of draws
+    :param hits: successes in population. 15 lands remaining in deck
+    :param k: successes we want in draws
+    :return: probability of successes in sample
     """
-    hpd = ss.hypergeom(populationSize, sampleSize, successesInPop)
-    return hpd.pmf(successesInSample)
+    hpd = ss.hypergeom(deckSize, drawSteps, hits)
+    return hpd.pmf(k)
 
 
-def hyperUpTo(populationSize, sampleSize, successesInPop, k):
+def hypergeoCDF(deckSize, drawSteps, hits, k):
     """
+    sums up hypergeometric PMF values to come up with cumulative density
     :return: probability of 1 to k successes in sample
     """
-    accumulatedProbability: float = 0
-    for i in range(1, k + 1):
-        partialProbability = \
-            hyperSingle(populationSize, sampleSize, successesInPop, i)
-        print(f'🐳{i}: {partialProbability}')
 
-        accumulatedProbability += partialProbability
+    # note this does not include 0 successes; we start at 1. otherwise we
+    # return 1 all the time
+    partialProbabilities: List[float] = \
+        [hypergeoPMF(deckSize, drawSteps, hits, x) for x
+         in range(1, k + 1)]
 
+    for i in range(len(partialProbabilities)):
+        print(f'🐳 {i+1} → {partialProbabilities[i]}')
 
-    return accumulatedProbability
-
-
-print(f'{hyperUpTo(33, 3, 15, 3)}')
+    return sum(partialProbabilities)
 
 
-# TODO understand the 3 expressions
-def hypergeometricPMF(N, A, n, x):
+print(f'{hypergeoCDF(33, 2, 15, 3)}')
+
+
+def hypergeoPlot(N, A, n):
     """
-    Probability Mass Function for Hypergeometric Distribution
+    Visualization of Hypergeometric Distribution for given parameters
     :param N: population size
     :param A: total number of desired items in N
     :param n: number of draws made from N
-    :param x: number of desired items in our draw of n items
-    :returns: PMF computed at x
+    :returns: Plot of Hypergeometric Distribution for given parameters
     """
-    a_choose_x = comb(A, x)
-    na_choose_x = comb(N - A, n - x)
-    n_choose_n = comb(N, n)
+    plt.style.use('dark_background')
 
-    return a_choose_x * na_choose_x / n_choose_n
+    x = np.arange(1, n + 1)
+    y = [hypergeoPMF(N, A, n, x) for x in range(1, n + 1)]
+
+    # probably should use accumulate from itertools
+    # → runningSum = list(accumulate(y))
+    runningSumOfY = [sum(y[:i+1]) for i in range(len(y))]
+
+    plt.plot(x, runningSumOfY, 'bo')
+    plt.vlines(x, 0, runningSumOfY, lw=2)
+    plt.xlabel('# of desired items in our draw')
+    plt.ylabel('Probabilities')
+    plt.title('Hypergeometric Distribution Plot')
+    plt.show()
+
+
+hypergeoPlot(33, 15, 3)
