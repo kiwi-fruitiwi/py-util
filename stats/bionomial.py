13e8be8dadf61820c0d698ba46bdf21aac418105
kiwi <zane.tian@gmail.com>
2023-12-02 05:08:38 -0800

    implement binomialCDF and comparisons with stats library

--- stats/bionomial.py
+++ stats/bionomial.py
@@ -5,7 +5,15 @@ from scipy.stats import binom
 import math
 
 
-def binomialPMF(n: int, p: float, k: int) -> float:
+def binomialPMF(k: int, n: int, p: float) -> float:
+	"""
+	:param k: number of successes desired
+	:param n: number of trials
+	:param p: probability of success
+	:return: probability of exactly k successes in n trials with success
+	probability p
+	"""
+
 	# each success has probability p^k*(1-p)^(n-k)
 	#	this means success chances multiplied by failure chances
 	#	e.g. 2 successes of 6 trials at 60%: .6²*.4⁴
@@ -25,16 +33,24 @@ def binomialPMF(n: int, p: float, k: int) -> float:
 	return probabilityOfEachSuccess * nChooseK
 
 
+def binomialCDF(k: int, n: int, p: float) -> float:
+	# sum up all binomialPMFs up to and including k
+	result = 0
+	for i in range(0, k+1):
+		result += binomialPMF(i, n, p)
+	return result
+
+
 # prevent imports from running this test code
 if __name__ == "__main__":
 	numTrials = 7  # Number of trials
 	prob = 0.35  # Probability of success
 	successes = 4  # Number of successes
 
-	pmf_result = binom.pmf(numTrials, prob, successes)
-	print("PMF:", pmf_result)
 
-
-	# calculate it manually
-	print(f'🥝PMF: {binomialPMF(numTrials, prob, successes)}')
+	# compare manual calculations with library ones
+	print(f'🐍 PMF: {binom.pmf(successes, numTrials, prob)}')
+	print(f'🥝 PMF: {binomialPMF(successes, numTrials, prob)}')
+	print(f'🐍 CDF: {binom.cdf(successes, numTrials, prob)}')
+	print(f'🥝 CDF: {binomialCDF(successes, numTrials, prob)}')
 
